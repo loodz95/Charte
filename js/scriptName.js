7374bb8f14932048daede9cd16eacf3d4143c485
OZIER Ludovic (SNCF VOYAGEURS / TECHNICENTRE DE PARIS NORD / EQUIPE EXAMENS JOUR) <9001074b@commun.ad.sncf.fr>
2022-09-12 10:28:32 +0200

    Favicon mis à jour

--- js/scriptName.js
+++ js/scriptName.js
@@ -6,27 +6,25 @@ let togg3 = document.getElementById("togg3");
 let d3 = document.getElementById("d3");
 
 togg1.addEventListener("click", () => {
-    if(getComputedStyle(d1).display != "none"){
-      d1.style.display = "none";
-    } else {
-      d1.style.display = "block";
-    }
-  })
+  if (getComputedStyle(d1).display != "none") {
+    d1.style.display = "none";
+  } else {
+    d1.style.display = "block";
+  }
+});
 
-  togg2.addEventListener("click", () => {
-    if(getComputedStyle(d2).display != "none"){
-      d2.style.display = "none";
-    } else {
-      d2.style.display = "block";
-    }
-  })
+togg2.addEventListener("click", () => {
+  if (getComputedStyle(d2).display != "none") {
+    d2.style.display = "none";
+  } else {
+    d2.style.display = "block";
+  }
+});
 
-  togg3.addEventListener("click", () => {
-    if(getComputedStyle(d3).display != "none"){
-      d3.style.display = "none";
-    } else {
-      d3.style.display = "block";
-    }
-  })
-
- 
+togg3.addEventListener("click", () => {
+  if (getComputedStyle(d3).display != "none") {
+    d3.style.display = "none";
+  } else {
+    d3.style.display = "block";
+  }
+});
